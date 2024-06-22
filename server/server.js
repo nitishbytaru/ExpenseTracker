27814d50cb5a3a376cb7f6a4a349368d76f9f13d
nitishbytaru <bndnitish24@gmail.com>
2024-06-22 11:07:12 +0530

    ready to deploy

--- server/server.js
+++ server/server.js
@@ -4,11 +4,19 @@ import dotenv from "dotenv";
 import db from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import sessionMiddleware from "./middleware/session.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "https://Expense_Tracker.onrender.com",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(sessionMiddleware);
 app.use("", userRoutes);
