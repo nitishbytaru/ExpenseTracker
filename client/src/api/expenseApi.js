3ae1dac7d2ef732ab163881a9a104a65f706936b
B Nitish Durga <137098123+nitishbytaru@users.noreply.github.com>
2024-06-22 13:17:49 +0530

    Update expenseApi.js

--- client/src/api/expenseApi.js
+++ client/src/api/expenseApi.js
@@ -2,51 +2,51 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-// api for fetching the expense history
+// API for fetching the expense history
 export const getHistory = async () => {
   try {
-    return await axios.get(`${API_URL}/history`, { withCredentials: true });
+    return await axios.get(`${API_URL}/api/history`, { withCredentials: true });
   } catch (error) {
     console.log(error);
     return error;
   }
 };
 
-// api for fetching the expense history with the date filter
+// API for fetching the expense history with the date filter
 export const getFilteredHistory = async (data) => {
   try {
-    return await axios.post(`${API_URL}/filteredHistory`, data, { withCredentials: true });
+    return await axios.post(`${API_URL}/api/filteredHistory`, data, { withCredentials: true });
   } catch (error) {
     console.log(error);
     return error;
   }
 };
 
-// api for sending form data
+// API for sending form data
 export const addExpense = async (Data) => {
   try {
-    await axios.post(`${API_URL}/expense`, Data, { withCredentials: true });
+    await axios.post(`${API_URL}/api/expense`, Data, { withCredentials: true });
   } catch (error) {
     console.log(error);
     return error;
   }
 };
 
-// api to delete transaction
+// API to delete transaction
 export const deleteTransaction = async (id) => {
   try {
-    await axios.delete(`${API_URL}/deleteTransaction/${id}`, { withCredentials: true });
+    await axios.delete(`${API_URL}/api/deleteTransaction/${id}`, { withCredentials: true });
   } catch (error) {
     console.log(error);
     return error;
   }
 };
 
-// api to update transaction
+// API to update transaction
 export const updateTransaction = async (id, Data) => {
   try {
     const { income, note, expense, transactionDate } = Data;
-    await axios.put(`${API_URL}/updateTransaction/${id}`, {
+    await axios.put(`${API_URL}/api/updateTransaction/${id}`, {
       income: parseInt(income),
       note,
       expense: parseInt(expense),
