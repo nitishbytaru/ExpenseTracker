f5ad8d6cf8ca4d76d5e8bb4fbd61c2b81f2efc7c
B Nitish Durga <137098123+nitishbytaru@users.noreply.github.com>
2024-07-10 17:39:33 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  return {
-    server: {
-      proxy: {
-        '/api': {
-          target: "https://expense-tracker-api-mu.vercel.app",
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
-    plugins: [react()],
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
