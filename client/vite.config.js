2ae14f45f578f1d78808e47edc389d5e8c2df5d7
B Nitish Durga <137098123+nitishbytaru@users.noreply.github.com>
2024-07-10 16:11:16 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
       proxy: {
         '/api': {
           target: env.VITE_API_BASE_URL,
-          changeOrigin: true,
+          // changeOrigin: true,
           secure: false,
         },
       },
