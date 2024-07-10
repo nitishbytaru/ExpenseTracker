bbbf56f6a103a5af39129deea5f76b5bf118a3c8
B Nitish Durga <137098123+nitishbytaru@users.noreply.github.com>
2024-07-10 17:44:13 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    plugins: [react()],
+  };
+});
