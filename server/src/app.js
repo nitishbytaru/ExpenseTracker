7fd1b032ffa76542b3fb9d8c5467fcb057fc06dc
B Nitish Durga <137098123+nitishbytaru@users.noreply.github.com>
2024-07-10 16:08:37 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -4,12 +4,13 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://expensetrackerclient.vercel.app",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://expensetrackerclient.vercel.app",
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
