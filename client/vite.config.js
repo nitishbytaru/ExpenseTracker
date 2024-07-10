e4e1e090e3420db0e5f98be75225d9fc9846b3b4
B Nitish Durga <137098123+nitishbytaru@users.noreply.github.com>
2024-07-10 19:31:26 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,24 @@
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd());
+//   return {
+//     server: {
+//       proxy: {
+//         '/api': {
+//           // target: "https://expense-tracker-api-mu.vercel.app",
+//           target: process.env.BACKEND_API_URL,
+//           changeOrigin: true,
+//           secure: false,
+//         },
+//       },
+//     },
+//     plugins: [react()],
+//   };
+// });
+
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -7,8 +28,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          // target: "https://expense-tracker-api-mu.vercel.app",
-          target: process.env.BACKEND_API_URL,
+          target: env.VITE_BACKEND_API_URL,
           changeOrigin: true,
           secure: false,
         },
