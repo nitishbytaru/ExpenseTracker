ed6021db577b3ef0e4784e212098524a9472e56a
B Nitish Durga <137098123+nitishbytaru@users.noreply.github.com>
2024-07-10 17:12:40 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -12,12 +12,6 @@ app.use(
   })
 );
 
-// Handle preflight requests
-app.options("*", cors({
-  origin: "https://expensetrackerclient.vercel.app",
-  credentials: true,
-}));
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
