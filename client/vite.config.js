8ee56af5f5bf36ff016abc86802e646d84c1dff6
nitishbytaru <bndnitish24@gmail.com>
2024-07-11 15:00:31 +0530

    ""

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  return {
-    server: {
-      proxy: {
-        '/api': {
-          target: "https://expensetracker-vbp3.onrender.com",
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
       },
     },
-    plugins: [react()],
-  };
+  },
+  plugins: [react()],
 });
